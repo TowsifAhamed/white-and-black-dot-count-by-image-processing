4a8b0e0993bb6c8177040749a0f440312d8c541e
Towsif Ahmed Labib <towsif.ahmed.labib@gmail.com>
2019-06-12 00:34:35 +0600

    Create white dot count.py

---
+++ white dot count.py
@@ -0,0 +1,20 @@
+import cv2
+path="white dot.jpg"
+gray = cv2.imread(path, 0)
+
+## threshold
+th, threshed = cv2.threshold(gray, 100, 255,cv2.THRESH_BINARY_INV|cv2.THRESH_OTSU)
+
+## findcontours
+cnts = cv2.findContours(threshed, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[-2]
+
+
+## filter by area
+s1= 3
+s2 = 20
+xcnts = []
+for cnt in cnts:
+    if s1<cv2.contourArea(cnt) <s2:
+        xcnts.append(cnt)
+
+print("\nDots number: {}".format(len(xcnts)))
